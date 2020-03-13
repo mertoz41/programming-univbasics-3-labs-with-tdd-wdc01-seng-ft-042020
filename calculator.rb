b3085a19c665b91644274f5739b5b969f6ad8c2c
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-03-13 18:24:51 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
 # Add your variables here
+first_number = 9
+puts first_number
+second_number = 8
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
